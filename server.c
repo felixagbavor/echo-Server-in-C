f2246c525755d1566eb367896ec4a4ca65f21a2b
SupremeO <agbavorfelix0@gmail.com>
2017-08-16 14:54:43 -0400

    Add files via upload

---
+++ server.c
@@ -0,0 +1,108 @@
+/* 
+ * echoserver.c - A simple connection-based echo server 
+ * usage: echoserver <port>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFSIZE 1024
+
+#if 0
+
+#endif
+
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  int listenfd, connfd, portno, clientlen;
+  struct sockaddr_in serveraddr;
+  struct sockaddr_in clientaddr; 
+  struct hostent *hostp;
+  char buf[BUFSIZE];
+  char *hostaddrp;
+  int optval;
+  int n;
+
+  /* check command line args */
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[1]);
+
+  /* socket: create a socket */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) 
+    error("ERROR opening socket");
+
+  optval = 1;
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+  /* build the server's internet address */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET; /* we are using the Internet */
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); /* accept reqs to any IP addr */
+  serveraddr.sin_port = htons((unsigned short)portno); /* port to listen on */
+
+  /* bind: associate the listening socket with a port */
+  if (bind(listenfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0) 
+    error("ERROR on binding");
+
+  /* listen: make it a listening socket ready to accept connection requests */
+  if (listen(listenfd, 5) < 0) /* allow 5 requests to queue up */ 
+    error("ERROR on listen");
+
+  /* main loop: wait for a connection request, echo input line, 
+     then close connection. */
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+    /* accept: wait for a connection request */
+    connfd = accept(listenfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (connfd < 0) 
+      error("ERROR on accept");
+    //10.250.29.50
+	/* gethostbyaddr: determine who sent the message */
+	
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      error("ERROR on gethostbyaddr");
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      error("ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n", 
+	   hostp->h_name, hostaddrp);
+    
+
+    /* read: read input string from the client */
+    bzero(buf, BUFSIZE);
+    n = read(connfd, buf, BUFSIZE);
+    if (n < 0) 
+      error("ERROR reading from socket");
+    printf("server received %d bytes: %s", n, buf);
+    
+	/* write: echo the input string back to the client */
+	printf("msg to client: ");
+	bzero(buf, BUFSIZE);
+    fgets(buf, BUFSIZE, stdin);
+    n = write(connfd, buf, strlen(buf));
+    if (n < 0) 
+      error("ERROR writing to socket");
+	}
+    close(connfd);
+  
+}
